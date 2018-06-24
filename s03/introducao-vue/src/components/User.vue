c231fe38e3ebd1502bb9e2486b32775200073999
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-06-24 16:57:24 -0300

    3-54 Criando primeiro componente

---
+++ s03/introducao-vue/src/components/User.vue
@@ -0,0 +1,22 @@
+<template>
+	<div id="user">
+		<h3>O usuário</h3>
+		<p>Meu nome é {{ username }}</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				username: 'Angelo'
+			}
+		}
+	}
+</script>
+
+<style>
+	p {
+		font-style: italic
+	}
+</style>
