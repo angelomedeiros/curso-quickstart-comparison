0d91553bf6373670e8fd99d62762b32995693f54
Angelo Medeiros Nóbrega <angelomedeiros3@gmail.com>
2018-06-24 18:39:52 -0300

    3-61 Alterando o modo do router para history (retira o # da uri)

--- s03/src/App.vue
+++ s03/src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
     <!-- <app-user></app-user> -->
+    <div>
+      <router-link to="/">Home</router-link>
+      <router-link to="/blog" >Blog</router-link>
+      <router-link to="/account">Account</router-link>
+    </div>
     <router-view> </router-view>
     <p>Teste</p>
   </div>
